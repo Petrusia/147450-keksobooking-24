f5c5f8234a4581b2cf70fdda5f97e25fa1c1267f
Petras Blaževičius <petrasblazevicius1@gmail.com>
2021-11-05 20:05:21 +0200

    4.9.4 Больше деталей

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ import {createAdd} from './utils/create-add';
 
 const ADS_NUMBER = 10;
 
+// eslint-disable-next-line no-unused-vars
 const similarAds = Array.from({length: ADS_NUMBER}, createAdd);
 
 
