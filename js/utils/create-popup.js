3c82b131fa5d84f3d0797c118478fc04fef77633
Petras Blaževičius <petrasblazevicius1@gmail.com>
2021-11-14 23:17:39 +0200

    11.13.1 Надо подкачаться

--- js/utils/create-popup.js
+++ js/utils/create-popup.js
@@ -16,32 +16,7 @@ const getOfferRooms = (rooms) => {
 
 const getOfferGuests = (guests) => guests === 1 ? '1 гостя' : `${guests} гостей`;
 
-const getAdsFeatures = (features) => {
-  const featuresFragment = document.createDocumentFragment();
-  features.forEach((feature) => {
-    const featureItem = document.createElement('li');
-    featureItem.classList.add('popup__feature', `popup__feature--${feature}`);
-    featuresFragment.appendChild(featureItem);
-  });
-  return featuresFragment;
-};
-
-const getAdsPhotos = (photos) => {
-  const photosFragment = document.createDocumentFragment();
-  photos.forEach((photo) => {
-    const image = document.createElement('img');
-    image.src = photo;
-    image.alt = 'Фотография жилья';
-    image.width = 45;
-    image.height = 40;
-    image.classList.add('popup__photo');
-    photosFragment.appendChild(image);
-  });
-  return photosFragment;
-};
-
 export const createPopup = (adsData) => {
-  const adsElementFragment = document.createDocumentFragment();
   const adsElement = document.querySelector('#card').content.querySelector('.popup').cloneNode(true);
   adsElement.querySelector('.popup__avatar').src = adsData.author.avatar;
   adsElement.querySelector('.popup__title').textContent = adsData.offer.title;
@@ -50,17 +25,35 @@ export const createPopup = (adsData) => {
   adsElement.querySelector('.popup__type').textContent = getOfferType(adsData.offer.type);
   adsElement.querySelector('.popup__text--capacity').textContent = `${getOfferRooms(adsData.offer.rooms)} для ${getOfferGuests(adsData.offer.guests)}`;
   adsElement.querySelector('.popup__text--time').textContent = `Заезд после ${adsData.offer.checkin}, выезд до ${adsData.offer.checkout}`;
-  const featuresContainer = adsElement.querySelector('.popup__features');
-  featuresContainer.innerHTML = '';
-  featuresContainer.appendChild(getAdsFeatures(adsData.offer.features));
-  adsElement.querySelector('.popup__description').textContent = adsData.offer.description;
-  const photosContainer = adsElement.querySelector('.popup__photos');
-  photosContainer.innerHTML = '';
-  photosContainer.appendChild(getAdsPhotos(adsData.offer.photos));
-  for (let index = 1; index < adsElement.children.length; index++) {
-    if (!adsElement.children[index].innerHTML) {
-      adsElement.children[index].remove();
-    }
+
+  const popupFeatures = adsElement.querySelector('.popup__features');
+
+  if(!adsData.offer.features) {
+    popupFeatures.remove();
+  }else {
+    adsData.offer.features.forEach((element) => {
+      const feature = document.createElement('li');
+      feature.classList.add('popup__feature', `popup__feature--${element}`);
+      popupFeatures.append(feature);
+    });
   }
-  return adsElementFragment.appendChild(adsElement);
+
+  const popupPhotos = adsElement.querySelector('.popup__photos');
+  const popupPhoto = popupPhotos.querySelector('.popup__photo');
+
+  if(!adsData.offer.photos) {
+    popupPhotos.remove();
+  } else {
+    adsData.offer.photos.forEach((photoSrc, index) => {
+      if(index === 0){
+        popupPhoto.src = adsData.offer.photos[0];
+      } else {
+        const popupPhotoItem = popupPhoto.cloneNode(true);
+        popupPhotoItem.src = photoSrc;
+        popupPhotos.appendChild(popupPhotoItem);
+      }
+    });
+  }
+  return adsElement;
 };
+
